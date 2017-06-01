52438829c47aac2a9ca666bff4393455ed18b0a6
yulu9206@gmail.com <yulu9206@Lus-MacBook-Air.local>
2017-05-31 22:49:07 -0700

    initial commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+
+
+def hello_world():
+	# return render_template('hello.html')
+	return "Hello World!"
+app.run(debug=True)
